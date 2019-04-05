d08aa3fe3b464a97fea45e43d6ba3d72925cd522
Beniamin Cazacu <cazacubeni@gmail.com>
2019-04-05 17:35:36 +0300

    Static files added

--- src/platforma_web/settings.py
+++ src/platforma_web/settings.py
@@ -123,6 +123,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static/media/'
+
 try:
    from .local_settings import *
 except:
